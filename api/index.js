06ace9ea46b43ba5a92fe8ab7e9cbe3b2239decc
sandip basa <sandipbasa18@gmail.com>
2025-03-16 22:10:38 +0530

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
+
+// Correctly access the environment variable
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-  });
+  console.log('Server is running on port 3000!');
+});
