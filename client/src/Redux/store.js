2255bb951a2c1f87bf059eb20557a884be8717d4
sandip basa <sandipbasa18@gmail.com>
2025-03-18 22:53:54 +0530

    install redux toolkit

---
+++ client/src/Redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+
+export const store = configureStore({
+  reducer: { user: userReducer }, // Add your reducers here
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check if needed
+    }),
+});
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider
+import './index.css';
+import App from './App.jsx';
+import { store } from './Redux/store.js'; // Import store
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}> {/* Wrap App with Provider and pass store */}
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
