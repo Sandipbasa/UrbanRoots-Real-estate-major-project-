4de09b6290f59b1299cba032258fe4f799e0099d
sandip basa <sandipbasa18@gmail.com>
2025-03-23 22:35:19 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config(); // Load environment variables from .env file
 
 // Correctly access the environment variable
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -18,15 +20,10 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
-
-app.use('/api/user',userRouter);
+app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
@@ -37,4 +34,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
